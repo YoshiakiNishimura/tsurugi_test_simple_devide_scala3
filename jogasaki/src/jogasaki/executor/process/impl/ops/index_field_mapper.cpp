9f88e8f945aa7f17dcc351f2a37fbc3ea422b2dc
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-04-22 13:08:52 +0900

    index_field

---
+++ jogasaki/src/jogasaki/executor/process/impl/ops/index_field_mapper.cpp
@@ -0,0 +1,195 @@
+/*
+ * Copyright 2018-2023 Project Tsurugi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "index_field_mapper.h"
+
+#include <ostream>
+#include <utility>
+#include <glog/logging.h>
+
+#include <jogasaki/error/error_info_factory.h>
+#include <jogasaki/index/field_info.h>
+#include <jogasaki/index/index_accessor.h>
+#include <jogasaki/kvs/coder.h>
+#include <jogasaki/kvs/readable_stream.h>
+#include <jogasaki/kvs/storage.h>
+#include <jogasaki/logging.h>
+#include <jogasaki/logging_helper.h>
+#include <jogasaki/status.h>
+#include <jogasaki/transaction_context.h>
+#include <jogasaki/utils/abort_transaction.h>
+#include <jogasaki/utils/handle_generic_error.h>
+#include <jogasaki/utils/handle_kvs_errors.h>
+#include <jogasaki/utils/modify_status.h>
+
+namespace jogasaki::executor::process::impl::ops {
+
+details::secondary_index_field_info::secondary_index_field_info(
+    meta::field_type type,
+    bool source_nullable,
+    kvs::coding_spec spec
+) :
+    type_(std::move(type)),
+    source_nullable_(source_nullable),
+    spec_(spec)
+{}
+
+index_field_mapper::index_field_mapper(
+    bool use_secondary,
+    std::vector<index::field_info> primary_key_fields,
+    std::vector<index::field_info> primary_value_fields,
+    std::vector<details::secondary_index_field_info> secondary_key_fields
+) :
+    use_secondary_(use_secondary),
+    primary_key_fields_(std::move(primary_key_fields)),
+    primary_value_fields_(std::move(primary_value_fields)),
+    secondary_key_fields_(std::move(secondary_key_fields))
+{}
+
+index_field_mapper::index_field_mapper(
+    std::vector<index::field_info> primary_key_fields,
+    std::vector<index::field_info> primary_value_fields,
+    std::vector<details::secondary_index_field_info> secondary_key_fields
+) :
+    index_field_mapper(
+        true,
+        std::move(primary_key_fields),
+        std::move(primary_value_fields),
+        std::move(secondary_key_fields)
+    )
+{}
+
+index_field_mapper::index_field_mapper(
+    std::vector<index::field_info> primary_key_fields,
+    std::vector<index::field_info> primary_value_fields
+) :
+    index_field_mapper(
+        false,
+        std::move(primary_key_fields),
+        std::move(primary_value_fields),
+        {}
+    )
+{}
+
+status index_field_mapper::process(
+    std::string_view key,
+    std::string_view value,
+    accessor::record_ref target,
+    kvs::storage& stg,
+    transaction_context& tx,
+    index_field_mapper::memory_resource* resource,
+    request_context& req_context,
+    decode_timings* timings
+) {
+    auto start = std::chrono::steady_clock::now();
+    std::string_view k{key};
+    std::string_view v{value};
+    if (use_secondary_) {
+        k = extract_primary_key(key);
+        if (auto res = find_primary_index(k, stg, tx, v, req_context); res != status::ok) {
+            return res;
+        }
+    }
+    timings->index_field_mapper_process += std::chrono::duration_cast<metrics::decode_timings::duration_type>(std::chrono::steady_clock::now() - start);
+    return populate_field_variables(k, v, target, resource/*,timings*/);
+}
+
+status index_field_mapper::consume_secondary_key_fields(
+    std::vector<details::secondary_index_field_info> const& fields,
+    kvs::readable_stream& stream
+) {
+    for(auto&& f : fields) {
+        kvs::coding_context ctx{};
+        if (f.source_nullable_) {
+            if(auto res = kvs::consume_stream_nullable(stream, f.type_, f.spec_, ctx); res != status::ok) {
+                return res;
+            }
+            continue;
+        }
+        if(auto res = kvs::consume_stream(stream, f.type_, f.spec_, ctx); res != status::ok) {
+            return res;
+        }
+    }
+    return status::ok;
+}
+
+std::string_view index_field_mapper::extract_primary_key(std::string_view key) {
+    kvs::readable_stream keys{key.data(), key.size()};
+    // consume key fields, then the rest is primary key
+    consume_secondary_key_fields(secondary_key_fields_, keys);
+    return keys.rest();
+}
+
+status index_field_mapper::find_primary_index(
+    std::string_view key,
+    kvs::storage& stg,
+    transaction_context& tx,
+    std::string_view& value_out,
+    request_context& req_context
+) {
+    std::string_view v{};
+    if(auto res = stg.content_get(tx, key, v); res != status::ok) {
+        std::stringstream ss{};
+        // we cannot use utils::modify_concurrent_operation_status here because missing entry is an inconsistency
+        // between primary/secondary and should be treated as an error.
+        if(res == status::concurrent_operation) {
+            // concurrent operation blocks finding primary entry - retry might change the situation
+            ss << "finding primary entry from secondary index entry failed due to concurrent operation";
+            set_error(req_context, error_code::blocked_by_concurrent_operation_exception , ss.str(), res);
+            utils::abort_transaction(tx);
+        } else if (res == status::not_found) {
+            // primary/secondary indices are not consistent
+            ss << "missing primary index entry corresponding to the secondary index entry";
+            res = status::err_inconsistent_index;
+            set_error(req_context, error_code::secondary_index_corruption_exception, ss.str(), res);
+            utils::abort_transaction(tx);
+        } else {
+            handle_kvs_errors(req_context, res);
+            handle_generic_error(req_context, res, error_code::sql_execution_exception);
+        }
+        if(! ss.str().empty()) {
+            VLOG_LP(log_error) << ss.str();
+        }
+        return res;
+    }
+    value_out = v;
+    return status::ok;
+}
+
+status index_field_mapper::populate_field_variables(
+    std::string_view key,
+    std::string_view value,
+    accessor::record_ref target,
+    index_field_mapper::memory_resource* resource/*,
+    decode_timings* timings */
+) {
+    //auto start = std::chrono::steady_clock::now();
+    kvs::readable_stream keys{key.data(), key.size()};
+    kvs::readable_stream values{value.data(), value.size()};
+    //timings->populate_field_variables += std::chrono::duration_cast<metrics::decode_timings::duration_type>(std::chrono::steady_clock::now() - start);
+    //auto start1 = std::chrono::steady_clock::now();
+    if(auto res = index::decode_fields(primary_key_fields_, keys, target, resource); res != status::ok) {
+        return res;
+    }
+    if(auto res = index::decode_fields(primary_value_fields_, values, target, resource); res != status::ok) {
+        return res;
+    }
+   // timings->decode_fields += std::chrono::duration_cast<metrics::decode_timings::duration_type>(std::chrono::steady_clock::now() - start1);
+    return status::ok;
+}
+
+}
+
+
---
+++ jogasaki/src/jogasaki/executor/process/impl/ops/index_field_mapper.h
@@ -0,0 +1,173 @@
+/*
+ * Copyright 2018-2023 Project Tsurugi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <string_view>
+#include <vector>
+
+#include <jogasaki/accessor/record_ref.h>
+#include <jogasaki/index/field_info.h>
+#include <jogasaki/kvs/coder.h>
+#include <jogasaki/kvs/storage.h>
+#include <jogasaki/memory/lifo_paged_memory_resource.h>
+#include <jogasaki/meta/field_type.h>
+#include <jogasaki/request_context.h>
+#include <jogasaki/status.h>
+#include <jogasaki/transaction_context.h>
+#include <jogasaki/utils/interference_size.h>
+#include <jogasaki/metrics/decode_timings.h>
+namespace jogasaki::executor::process::impl::ops {
+using jogasaki::metrics::decode_timings;
+namespace details {
+
+/**
+ * @brief secondary index field info
+ * @details mapper uses these fields to extract the primary key from secondary key
+ */
+struct cache_align secondary_index_field_info {
+
+    /**
+     * @brief create new field information
+     * @param type type of the field
+     * @param source_nullable whether the target field is nullable or not
+     * @param spec the spec of the target field used for encode/decode
+     */
+    secondary_index_field_info(
+        meta::field_type type,
+        bool source_nullable,
+        kvs::coding_spec spec
+    );
+
+    meta::field_type type_{}; //NOLINT
+    bool source_nullable_{}; //NOLINT
+    kvs::coding_spec spec_{}; //NOLINT
+};
+
+}  // namespace details
+
+/**
+ * @brief index fields mapper object
+ * @details this object knows the mapping from secondary index (if any) to primary, receives the key/value and fill
+ * relation fields by resolving the primary key and identifying field values on the primary index.
+ */
+class index_field_mapper {
+public:
+    using memory_resource = memory::lifo_paged_memory_resource;
+
+    /**
+     * @brief create empty object
+     */
+    index_field_mapper() = default;
+
+    /**
+     * @brief common constructor for both cases on whether secondary index is used or not
+     * @param use_secondary whether secondary index is used
+     * @param primary_key_fields info. on primary index key fields
+     * @param primary_value_fields info. on primary index value fields
+     * @param secondary_key_fields info. on secondary index key fields
+     */
+    index_field_mapper(
+        bool use_secondary,
+        std::vector<index::field_info> primary_key_fields,
+        std::vector<index::field_info> primary_value_fields,
+        std::vector<details::secondary_index_field_info> secondary_key_fields
+    );
+
+    /**
+     * @brief create new object using secondary
+     * @param primary_key_fields info. on primary index key fields
+     * @param primary_value_fields info. on primary index value fields
+     * @param secondary_key_fields info. on secondary index key fields
+     */
+    index_field_mapper(
+        std::vector<index::field_info> primary_key_fields,
+        std::vector<index::field_info> primary_value_fields,
+        std::vector<details::secondary_index_field_info> secondary_key_fields
+    );
+
+    /**
+     * @brief create new object without secondary
+     * @param primary_key_fields info. on primary index key fields
+     * @param primary_value_fields info. on primary index value fields
+     */
+    index_field_mapper(
+        std::vector<index::field_info> primary_key_fields,
+        std::vector<index::field_info> primary_value_fields
+    );
+
+    /**
+     * @brief process input record, map key/value and fill the variables accessing the secondary index if necessary
+     * @details this function identifies the primary index record and fills the variables. If error occurs,
+     * `req_context` is filled with error_info and error status code is returned
+     * @param key the key of the input record (either primary or secondary)
+     * @param value the value of the input record (empty if secondary)
+     * @param target the record reference to fill the variables
+     * @param stg the primary index storage
+     * @param tx the transaction context
+     * @param resource the memory resource to allocate temporary buffers
+     * @param req_context the request context to report errors (nullptr if reporting is not necessary)
+     * @return status::ok if the operation is successful
+     * @return error status code otherwise
+     */
+    [[nodiscard]] status process(
+        std::string_view key,
+        std::string_view value,
+        accessor::record_ref target,
+        kvs::storage& stg,
+        transaction_context& tx,
+        memory_resource* resource,
+        request_context& req_context,
+        decode_timings* timings = nullptr
+    );
+
+private:
+    bool use_secondary_{};
+    std::vector<index::field_info> primary_key_fields_{};
+    std::vector<index::field_info> primary_value_fields_{};
+    std::vector<details::secondary_index_field_info> secondary_key_fields_{};
+
+    status consume_secondary_key_fields(
+        std::vector<details::secondary_index_field_info> const& fields,
+        kvs::readable_stream& stream
+    );
+
+    std::string_view extract_primary_key(
+        std::string_view key
+    );
+
+    status find_primary_index(
+        std::string_view key,
+        kvs::storage& stg,
+        transaction_context& tx,
+        std::string_view& value_out,
+        request_context& req_context
+    );
+
+    status populate_field_variables(
+        std::string_view key,
+        std::string_view value,
+        accessor::record_ref target,
+        memory_resource* resource
+	/*,
+        decode_timings* timings = nullptr
+	*/
+    );
+
+};
+
+}
+
+
