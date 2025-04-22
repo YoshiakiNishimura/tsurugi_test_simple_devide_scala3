f915334ae14b79997ecf1b2ffdabd788c923ae26
Yoshiaki Nishimura <nishimura@nautilus-technologies.com>
2025-04-22 14:08:37 +0900

    decode

--- jogasaki/include/jogasaki/metrics/decode_timings.h
+++ jogasaki/include/jogasaki/metrics/decode_timings.h
@@ -6,9 +6,9 @@ namespace jogasaki::metrics {
 
 struct decode_timings {
     using duration_type              = std::chrono::nanoseconds;
-    std::chrono::milliseconds start_ms           = duration_type::zero();
-    std::chrono::milliseconds end_ms             = duration_type::zero();
-    std::chrono::milliseconds duration_ms        = duration_type::zero();
+    std::chrono::milliseconds start_ms           = std::chrono::milliseconds::zero();
+    std::chrono::milliseconds end_ms             = std::chrono::milliseconds::zero();
+    std::chrono::milliseconds duration_ms        = std::chrono::milliseconds::zero();
     duration_type loop_time          = duration_type::zero();
     duration_type cancel_check       = duration_type::zero();
     duration_type kvs_next           = duration_type::zero();
@@ -17,6 +17,9 @@ struct decode_timings {
     duration_type field_process      = duration_type::zero();
     duration_type downstream_process = duration_type::zero();
     duration_type yield_check        = duration_type::zero();
+    duration_type index_field_mapper_process = duration_type::zero();
+    duration_type populate_field_variables        = duration_type::zero();
+    duration_type decode_fields        = duration_type::zero();
     size_t loop_count          = 0;
     void dump(std::ostream& out) const noexcept { out << "decode_timings:\n"; }
     void dump_csv(std::ostream& out) const noexcept {
@@ -27,7 +30,8 @@ struct decode_timings {
             << loop_time.count() << "," << unmeasured_time << "," << cancel_check.count()
             << "," << kvs_next.count() << "," << read_key_ms.count() << "," << read_value.count()
             << "," << field_process.count() << "," << downstream_process.count() << ","
-            << yield_check.count() << "," << loop_count << std::endl;
+            << yield_check.count() << "," << loop_count << ","<< index_field_mapper_process.count()
+	    << "," << populate_field_variables.count() << "," << decode_fields.count() << std::endl;
     }
 };
 
